5097a13a24a331717d79897de2aead068785a7f8
Gabriela Alonso <ealoama119@g.educaand.es>
2024-11-20 09:03:59 +0100

    Cambios al fusionar rama mejoras

--- src/Coche.java
+++ src/Coche.java
@@ -49,9 +49,7 @@ public class Coche {
     public void apagar() {
         System.out.println("El coche está apagado.");
     }
-    public void apagar() {
-        System.out.println("El coche se apaga de manera segura.");
-    }
+
 
 
 }
